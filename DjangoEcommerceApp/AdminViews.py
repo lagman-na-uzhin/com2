bc04b7698bdb6f770d741055ec9b263fc19b119d
lagman-na-uzhin <115121930+lagman-na-uzhin@users.noreply.github.com>
2023-04-17 01:28:23 +0600

    pagination, manageMerchant, searchSort,

--- DjangoEcommerceApp/AdminViews.py
+++ DjangoEcommerceApp/AdminViews.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, CreateView, UpdateView, DetailView
-from DjangoEcommerceApp.models import Categories, SubCategories, CustomUser, MerchantUser
+from django.views.generic import ListView,CreateView,UpdateView,DetailView
+from DjangoEcommerceApp.models import Categories,SubCategories,CustomUser,MerchantUser
 from django.contrib.messages.views import SuccessMessageMixin
 from django.core.files.storage import FileSystemStorage
 from django.contrib.messages.views import messages
@@ -9,188 +9,178 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.db.models import Q
 
-
 @login_required(login_url="/admin/")
 def admin_home(request):
-    return render(request, "admin_templates/home.html")
-
+    return render(request,"admin_templates/home.html")
 
 class CategoriesListView(ListView):
-    model = Categories
-    template_name = "admin_templates/category_list.html"
-    paginate_by = 3
+    model=Categories
+    template_name="admin_templates/category_list.html"
+    paginate_by=3
 
     def get_queryset(self):
-        filter_val = self.request.GET.get("filter", "")
-        order_by = self.request.GET.get("orderby", "id")
-        if filter_val != "":
-            cat = Categories.objects.filter(
-                Q(title__contains=filter_val) | Q(description__contains=filter_val)).order_by(order_by)
+        filter_val=self.request.GET.get("filter","")
+        order_by=self.request.GET.get("orderby","id")
+        if filter_val!="":
+            cat=Categories.objects.filter(Q(title__contains=filter_val) | Q(description__contains=filter_val)).order_by(order_by)
         else:
-            cat = Categories.objects.all().order_by(order_by)
+            cat=Categories.objects.all().order_by(order_by)
 
         return cat
 
-    def get_context_data(self, **kwargs):
-        context = super(CategoriesListView, self).get_context_data(**kwargs)
-        context["filter"] = self.request.GET.get("filter", "")
-        context["orderby"] = self.request.GET.get("orderby", "id")
-        context["all_table_fields"] = Categories._meta.get_fields()
+    def get_context_data(self,**kwargs):
+        context=super(CategoriesListView,self).get_context_data(**kwargs)
+        context["filter"]=self.request.GET.get("filter","")
+        context["orderby"]=self.request.GET.get("orderby","id")
+        context["all_table_fields"]=Categories._meta.get_fields()
         return context
 
 
-class CategoriesCreate(SuccessMessageMixin, CreateView):
-    model = Categories
-    success_message = "Category Added!"
-    fields = "__all__"
-    template_name = "admin_templates/category_create.html"
+class CategoriesCreate(SuccessMessageMixin,CreateView):
+    model=Categories
+    success_message="Category Added!"
+    fields="__all__"
+    template_name="admin_templates/category_create.html"
 
-
-class CategoriesUpdate(SuccessMessageMixin, UpdateView):
-    model = Categories
-    success_message = "Category Updated!"
-    fields = "__all__"
-    template_name = "admin_templates/category_update.html"
+class CategoriesUpdate(SuccessMessageMixin,UpdateView):
+    model=Categories
+    success_message="Category Updated!"
+    fields="__all__"
+    template_name="admin_templates/category_update.html"
 
 
 class SubCategoriesListView(ListView):
-    model = SubCategories
-    template_name = "admin_templates/sub_category_list.html"
-    paginate_by = 3
+    model=SubCategories
+    template_name="admin_templates/sub_category_list.html"
+    paginate_by=3
 
     def get_queryset(self):
-        filter_val = self.request.GET.get("filter", "")
-        order_by = self.request.GET.get("orderby", "id")
-        if filter_val != "":
-            cat = SubCategories.objects.filter(
-                Q(title__contains=filter_val) | Q(description__contains=filter_val)).order_by(order_by)
+        filter_val=self.request.GET.get("filter","")
+        order_by=self.request.GET.get("orderby","id")
+        if filter_val!="":
+            cat=SubCategories.objects.filter(Q(title__contains=filter_val) | Q(description__contains=filter_val)).order_by(order_by)
         else:
-            cat = SubCategories.objects.all().order_by(order_by)
+            cat=SubCategories.objects.all().order_by(order_by)
 
         return cat
 
-    def get_context_data(self, **kwargs):
-        context = super(SubCategoriesListView, self).get_context_data(**kwargs)
-        context["filter"] = self.request.GET.get("filter", "")
-        context["orderby"] = self.request.GET.get("orderby", "id")
-        context["all_table_fields"] = SubCategories._meta.get_fields()
+    def get_context_data(self,**kwargs):
+        context=super(SubCategoriesListView,self).get_context_data(**kwargs)
+        context["filter"]=self.request.GET.get("filter","")
+        context["orderby"]=self.request.GET.get("orderby","id")
+        context["all_table_fields"]=SubCategories._meta.get_fields()
         return context
 
 
-class SubCategoriesCreate(SuccessMessageMixin, CreateView):
-    model = SubCategories
-    success_message = "Sub Category Added!"
-    fields = "__all__"
-    template_name = "admin_templates/sub_category_create.html"
+class SubCategoriesCreate(SuccessMessageMixin,CreateView):
+    model=SubCategories
+    success_message="Sub Category Added!"
+    fields="__all__"
+    template_name="admin_templates/sub_category_create.html"
 
-
-class SubCategoriesUpdate(SuccessMessageMixin, UpdateView):
-    model = SubCategories
-    success_message = "Sub Category Updated!"
-    fields = "__all__"
-    template_name = "admin_templates/sub_category_update.html"
-
+class SubCategoriesUpdate(SuccessMessageMixin,UpdateView):
+    model=SubCategories
+    success_message="Sub Category Updated!"
+    fields="__all__"
+    template_name="admin_templates/sub_category_update.html"
 
 class MerchantUserListView(ListView):
-    model = MerchantUser
-    template_name = "admin_templates/merchant_list.html"
-    paginate_by = 3
+    model=MerchantUser
+    template_name="admin_templates/merchant_list.html"
+    paginate_by=3
 
     def get_queryset(self):
-        filter_val = self.request.GET.get("filter", "")
-        order_by = self.request.GET.get("orderby", "id")
-        if filter_val != "":
-            cat = MerchantUser.objects.filter(
-                Q(auth_user_id__first_name__contains=filter_val) | Q(auth_user_id__last_name__contains=filter_val) | Q(
-                    auth_user_id__email__contains=filter_val) | Q(
-                    auth_user_id__username__contains=filter_val)).order_by(order_by)
+        filter_val=self.request.GET.get("filter","")
+        order_by=self.request.GET.get("orderby","id")
+        if filter_val!="":
+            cat=MerchantUser.objects.filter(Q(auth_user_id__first_name__contains=filter_val) |Q(auth_user_id__last_name__contains=filter_val) | Q(auth_user_id__email__contains=filter_val) | Q(auth_user_id__username__contains=filter_val)).order_by(order_by)
         else:
-            cat = MerchantUser.objects.all().order_by(order_by)
+            cat=MerchantUser.objects.all().order_by(order_by)
 
         return cat
 
-    def get_context_data(self, **kwargs):
-        context = super(MerchantUserListView, self).get_context_data(**kwargs)
-        context["filter"] = self.request.GET.get("filter", "")
-        context["orderby"] = self.request.GET.get("orderby", "id")
-        context["all_table_fields"] = MerchantUser._meta.get_fields()
+    def get_context_data(self,**kwargs):
+        context=super(MerchantUserListView,self).get_context_data(**kwargs)
+        context["filter"]=self.request.GET.get("filter","")
+        context["orderby"]=self.request.GET.get("orderby","id")
+        context["all_table_fields"]=MerchantUser._meta.get_fields()
         return context
 
 
-class MerchantUserCreateView(SuccessMessageMixin, CreateView):
-    template_name = "admin_templates/merchant_create.html"
-    model = CustomUser
-    fields = ["first_name", "last_name", "email", "username", "password"]
+class MerchantUserCreateView(SuccessMessageMixin,CreateView):
+    template_name="admin_templates/merchant_create.html"
+    model=CustomUser
+    fields=["first_name","last_name","email","username","password"]
 
-    def form_valid(self, form):
-        # Saving Custom User Object for Merchant User
-        user = form.save(commit=False)
-        user.is_active = True
-        user.user_type = 3
+    def form_valid(self,form):
+
+        #Saving Custom User Object for Merchant User
+        user=form.save(commit=False)
+        user.is_active=True
+        user.user_type=3
         user.set_password(form.cleaned_data["password"])
         user.save()
 
-        # Saving Merchant user
-        profile_pic = self.request.FILES["profile_pic"]
-        fs = FileSystemStorage()
-        filename = fs.save(profile_pic.name, profile_pic)
-        profile_pic_url = fs.url(filename)
+        #Saving Merchant user
+        profile_pic=self.request.FILES["profile_pic"]
+        fs=FileSystemStorage()
+        filename=fs.save(profile_pic.name,profile_pic)
+        profile_pic_url=fs.url(filename)
 
-        user.merchantuser.profile_pic = profile_pic_url
-        user.merchantuser.company_name = self.request.POST.get("company_name")
-        user.merchantuser.gst_details = self.request.POST.get("gst_details")
-        user.merchantuser.address = self.request.POST.get("address")
-        is_added_by_admin = False
+        user.merchantuser.profile_pic=profile_pic_url
+        user.merchantuser.company_name=self.request.POST.get("company_name")
+        user.merchantuser.gst_details=self.request.POST.get("gst_details")
+        user.merchantuser.address=self.request.POST.get("address")
+        is_added_by_admin=False
 
-        if self.request.POST.get("is_added_by_admin") == "on":
-            is_added_by_admin = True
+        if self.request.POST.get("is_added_by_admin")=="on":
+            is_added_by_admin=True
 
-        user.merchantuser.is_added_by_admin = is_added_by_admin
+        user.merchantuser.is_added_by_admin=is_added_by_admin
         user.save()
-        messages.success(self.request, "Merchant User Created")
+        messages.success(self.request,"Merchant User Created")
         return HttpResponseRedirect(reverse("merchant_list"))
 
+class MerchantUserUpdateView(SuccessMessageMixin,UpdateView):
+    template_name="admin_templates/merchant_update.html"
+    model=CustomUser
+    fields=["first_name","last_name","email","username","password"]
 
-class MerchantUserUpdateView(SuccessMessageMixin, UpdateView):
-    template_name = "admin_templates/merchant_update.html"
-    model = CustomUser
-    fields = ["first_name", "last_name", "email", "username", "password"]
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        merchantuser = MerchantUser.objects.get(auth_user_id=self.object.pk)
-        context["merchantuser"] = merchantuser
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        merchantuser=MerchantUser.objects.get(auth_user_id=self.object.pk)
+        context["merchantuser"]=merchantuser
         return context
 
-    def form_valid(self, form):
+    def form_valid(self,form):
 
-        # Saving Custom User Object for Merchant User
-        user = form.save(commit=False)
+        #Saving Custom User Object for Merchant User
+        user=form.save(commit=False)
         user.set_password(form.cleaned_data["password"])
         user.save()
 
-        # Saving Merchant user
-        merchantuser = MerchantUser.objects.get(auth_user_id=user.id)
-        if self.request.FILES.get("profile_pic", False):
-            profile_pic = self.request.FILES["profile_pic"]
-            fs = FileSystemStorage()
-            filename = fs.save(profile_pic.name, profile_pic)
-            profile_pic_url = fs.url(filename)
-            merchantuser.profile_pic = profile_pic_url
+        #Saving Merchant user
+        merchantuser=MerchantUser.objects.get(auth_user_id=user.id)
+        if self.request.FILES.get("profile_pic",False):
+            profile_pic=self.request.FILES["profile_pic"]
+            fs=FileSystemStorage()
+            filename=fs.save(profile_pic.name,profile_pic)
+            profile_pic_url=fs.url(filename)
+            merchantuser.profile_pic=profile_pic_url
 
-        merchantuser.company_name = self.request.POST.get("company_name")
-        merchantuser.gst_details = self.request.POST.get("gst_details")
-        merchantuser.address = self.request.POST.get("address")
-        is_added_by_admin = False
+        merchantuser.company_name=self.request.POST.get("company_name")
+        merchantuser.gst_details=self.request.POST.get("gst_details")
+        merchantuser.address=self.request.POST.get("address")
+        is_added_by_admin=False
 
-        if self.request.POST.get("is_added_by_admin") == "on":
-            is_added_by_admin = True
+        if self.request.POST.get("is_added_by_admin")=="on":
+            is_added_by_admin=True
 
-        merchantuser.is_added_by_admin = is_added_by_admin
+        merchantuser.is_added_by_admin=is_added_by_admin
         merchantuser.save()
-        messages.success(self.request, "Merchant User Updated")
+        messages.success(self.request,"Merchant User Updated")
         return HttpResponseRedirect(reverse("merchant_list"))
 
-
+        
 
 
