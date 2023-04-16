a633316cead5884a482677df3485f83e8dc12170
lagman-na-uzhin <115121930+lagman-na-uzhin@users.noreply.github.com>
2023-04-16 21:37:06 +0600

    pagin

--- DjangoEcommerceApp /admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from DjangoEcommerceApp.models import Categories,SubCategories
-
-# Register your models here.
-admin.site.register(Categories)
-admin.site.register(SubCategories)
--- DjangoEcommerceApp /apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoecommerceappConfig(AppConfig):
-    name = 'DjangoEcommerceApp'
